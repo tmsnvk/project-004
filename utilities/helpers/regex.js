4c1e39e9659e904d519295e77856a04fbf1d5bd3
tmsnvk <contact@tamasnovak.net>
2020-11-07 18:36:39 +0100

    heroku push

--- utilities/helpers/regex.js
+++ utilities/helpers/regex.js
@@ -1,4 +1,4 @@
-const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/i;
 const regexUsername = /^[A-Za-z0-9 ]+$/i;
 
 module.exports = { regexEmail, regexUsername };
